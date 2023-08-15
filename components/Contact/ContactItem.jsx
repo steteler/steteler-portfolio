65ed8f36bf9671ed39e9df7ee148be1e7e9d25d7
kikuti <joaovictorkikuti@gmail.com>
2023-08-15 17:41:00 -0300

    refactor(components/contact): removido propriedades desnecessárias dos componentes contacts

--- components/Contact/ContactItem.jsx
+++ components/Contact/ContactItem.jsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function ContactItem({
-  link, src, alt, width, height,
+  link, src, alt,
 }) {
   return (
     <Link href={link}>
       <Image
         src={src}
         alt={alt}
-        width={width}
-        height={height}
+        width="36"
+        height="36"
       />
     </Link>
   );
