71a96900e530205413c720837e3fd9e11537f7ca
kikuti <joaovictorkikuti@gmail.com>
2023-08-15 18:07:33 -0300

    feat(pages/document): adicionado comando para converter para utf8

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,6 +13,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Inter&family=Raleway&display=swap" rel="stylesheet" />
+        <meta charset="UTF-8" />
       </Head>
       <body>
         <Main />
