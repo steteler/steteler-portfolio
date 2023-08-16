8f07c07aa925a62c720b43f3fc137ba2f4d1f71b
kikuti <joaovictorkikuti@gmail.com>
2023-08-15 22:51:20 -0300

    refactor(components/title): adicionado valor padrão a propriedade do componente title

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,6 +1,6 @@
 import styles from '@styles/Title.module.css';
 
-export default function Title({ text }) {
+export default function Title({ text = 'title' }) {
   return (
     <h1 className={styles.component__title}>{text}</h1>
   );
