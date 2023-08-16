cae01b7a4f3175e2c92812a207089aeef4e3f452
kikuti <joaovictorkikuti@gmail.com>
2023-08-15 22:52:24 -0300

    fix(pages/document): arrumado nome das propriedades
    n

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,9 +11,9 @@ export default function Document() {
       <Head>
         <title>Steteler Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Inter&family=Raleway&display=swap" rel="stylesheet" />
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
       </Head>
       <body>
         <Main />
