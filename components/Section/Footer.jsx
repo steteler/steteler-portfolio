cec86b7f5b5854ebe3278fd33544ed0b1b98d5ac
kikuti <joaovictorkikuti@gmail.com>
2023-08-19 20:55:22 -0300

    refactor(components/section): removido tags desnecessárias no componente footer
    após a refatoração algumas tags não eram mais necessárias, elas foram removidas

--- components/Section/Footer.jsx
+++ components/Section/Footer.jsx
@@ -3,16 +3,14 @@ import styles from '@styles/Section/Footer.module.css';
 
 export default function Footer() {
   return (
-    <section className={styles.footer} id="footer">
-      <div className={styles['section-default-config']}>
-        <p className={styles.footer__text}>
-          Feito com muito ☕
-          <br />
-          por João Victor Steteler
-        </p>
-        <p className={styles['footer__text-warn']}>código, design, referências e documentação do site no meu github oficial</p>
-        <ContactContainer />
-      </div>
+    <section id={styles.footer}>
+      <p className={styles.footer__text}>
+        Feito com muito ☕
+        <br />
+        por João Victor Steteler
+      </p>
+      <p className={styles['footer__text-warn']}>código, design, referências e documentação do site no meu github oficial</p>
+      <ContactContainer align="center" />
     </section>
   );
 }
