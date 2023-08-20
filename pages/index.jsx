3ca7e8e645820d0c5a7ca74b8b2c5a3d19ad88ec
kikuti <joaovictorkikuti@gmail.com>
2023-08-19 21:00:21 -0300

    refactor(pages/index): adicionado div para separar as sections do componente divider

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
+import Divider from '@components/Divider.jsx';
 import About from '@components/Section/About.jsx';
 import Project from '@components/Section/Project.jsx';
-import Skill from '@components/Section/Skill.jsx';
+import Skill from '@components/Section/Skill/Skill.jsx';
 import Experience from '@components/Section/Experience.jsx';
 import Footer from '@components/Section/Footer.jsx';
 import Home from '@components/Section/Home.jsx';
@@ -13,12 +14,29 @@ export default function Index() {
         <title>Steteler Portfolio</title>
       </Head>
       <main>
-        <Home />
-        <About />
-        <Project />
-        <Skill />
-        <Experience />
-        <Footer />
+        <div className="section-container home-section">
+          <Home />
+          <Divider />
+        </div>
+        <div className="section-container">
+          <About />
+          <Divider />
+        </div>
+        <div className="section-container">
+          <Project />
+          <Divider />
+        </div>
+        <div className="section-container">
+          <Skill />
+          <Divider />
+        </div>
+        <div className="section-container">
+          <Experience />
+          <Divider />
+        </div>
+        <div className="section-container">
+          <Footer />
+        </div>
       </main>
     </>
   );
