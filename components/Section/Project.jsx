46144657c300d6f18f1dfdb3e0c4b0f7ac2b6f0b
kikuti <joaovictorkikuti@gmail.com>
2023-08-19 22:22:26 -0300

    refactor(components/section): refatorado estilização e estrutura html do componente project

--- components/Section/Project.jsx
+++ components/Section/Project.jsx
@@ -1,97 +1,95 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Divider from '@components/Divider.jsx';
 import Title from '@components/Title.jsx';
 import styles from '@styles/Section/Project.module.css';
 import projectTemplate from '@public/project-template.png';
 
 export default function Project() {
   return (
-    <section className={styles.project} id="project">
-      <div className={styles['section-default-config']}>
-        <Title text="projetos" />
-        <details className={styles['project-container']}>
-          <summary data-close="Mostrar Filtros" data-open="Esconder Filtros" className={styles['project__filter-btn']} />
-          <div>
-            <h4>linguagem</h4>
-            <div className={styles['project__filter-container']}>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 1</button>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 2</button>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 3</button>
-            </div>
+    <section id="project">
+      <Title text="projetos" />
+      <details className={styles['project__filter-container']}>
+        <summary
+          data-close="Mostrar Filtros"
+          data-open="Esconder Filtros"
+          className={styles.project__btn}
+        />
+        <div>
+          <h4 className={styles['project__filter-title']}>linguagem</h4>
+          <div className={styles.project__filter}>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 1</button>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 2</button>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 3</button>
           </div>
-          <div>
-            <h4>banco de dados</h4>
-            <div className={styles['project__filter-container']}>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 1</button>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 2</button>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 3</button>
-            </div>
+        </div>
+        <div>
+          <h4 className={styles['project__filter-title']}>banco de dados</h4>
+          <div className={styles.project__filter}>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 1</button>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 2</button>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 3</button>
           </div>
-          <div>
-            <h4>ferramenta</h4>
-            <div className={styles['project__filter-container']}>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 1</button>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 2</button>
-              <button type="button" className={styles['project__filter-item']}>Opção de Filtro 3</button>
-            </div>
+        </div>
+        <div>
+          <h4 className={styles['project__filter-title']}>ferramenta</h4>
+          <div className={styles.project__filter}>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 1</button>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 2</button>
+            <button type="button" className={styles.project__filter__item}>Opção de Filtro 3</button>
           </div>
-        </details>
-
-        <section className={styles['project-list']}>
-          <div className={styles['project-item']}>
-            <Image
-              src={projectTemplate}
-              className={styles['project-item__image']}
-              alt="Project1"
-              priority="false"
-              quality={100}
-            />
-            <Link href="#project" className={styles['project-item__link']}>link projeto</Link>
-          </div>
-          <div className={styles['project-item']}>
-            <Image
-              src={projectTemplate}
-              className={styles['project-item__image']}
-              alt="Project1"
-              priority="false"
-              quality={100}
-            />
-            <Link href="#project" className={styles['project-item__link']}>link projeto</Link>
-          </div>
-          <div className={styles['project-item']}>
-            <Image
-              src={projectTemplate}
-              className={styles['project-item__image']}
-              alt="Project1"
-              priority="false"
-              quality={100}
-            />
-            <Link href="#project" className={styles['project-item__link']}>link projeto</Link>
-          </div>
-          <div className={styles['project-item']}>
-            <Image
-              src={projectTemplate}
-              className={styles['project-item__image']}
-              alt="Project1"
-              priority="false"
-              quality={100}
-            />
-            <Link href="#project" className={styles['project-item__link']}>link projeto</Link>
-          </div>
-          <div className={styles['project-item']}>
-            <Image
-              src={projectTemplate}
-              className={styles['project-item__image']}
-              alt="Project1"
-              priority="false"
-              quality={100}
-            />
-            <Link href="#project" className={styles['project-item__link']}>link projeto</Link>
-          </div>
-        </section>
-      </div>
-      <Divider />
+        </div>
+      </details>
+      <section className={styles['project-list']}>
+        <div className={styles['project-list__item']}>
+          <Image
+            src={projectTemplate}
+            className={styles['project-list__item__image']}
+            alt="Project1"
+            quality={100}
+          />
+          <Link href="#project" className={styles['project-list__item__link']}>link projeto</Link>
+        </div>
+        <div className={styles['project-list__item']}>
+          <Image
+            src={projectTemplate}
+            className={styles['project-list__item__image']}
+            alt="Project1"
+            priority="false"
+            quality={100}
+          />
+          <Link href="#project" className={styles['project-list__item__link']}>link projeto</Link>
+        </div>
+        <div className={styles['project-list__item']}>
+          <Image
+            src={projectTemplate}
+            className={styles['project-list__item__image']}
+            alt="Project1"
+            priority="false"
+            quality={100}
+          />
+          <Link href="#project" className={styles['project-list__item__link']}>link projeto</Link>
+        </div>
+        <div className={styles['project-list__item']}>
+          <Image
+            src={projectTemplate}
+            className={styles['project-list__item__image']}
+            alt="Project1"
+            priority="false"
+            quality={100}
+          />
+          <Link href="#project" className={styles['project-list__item__link']}>link projeto</Link>
+        </div>
+        <div className={styles['project-list__item']}>
+          <Image
+            src={projectTemplate}
+            className={styles['project-list__item__image']}
+            alt="Project1"
+            priority="false"
+            quality={100}
+          />
+          <Link href="#project" className={styles['project-list__item__link']}>link projeto</Link>
+        </div>
+      </section>
     </section>
   );
 }
