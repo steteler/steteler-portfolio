3402c0d05bd5c8886870b61ddef813770be7f10f
kikuti <joaovictorkikuti@gmail.com>
2023-08-20 12:02:19 -0300

    feat(components/section): adicionado imagem ilustrativa no componente home e criado style responsivo
    criado versão para tablet/desktop do componente home

--- components/Section/Home.jsx
+++ components/Section/Home.jsx
@@ -1,28 +1,42 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import ContactContainer from '@components/Contact/ContactContainer.jsx';
 import styles from '@styles/Section/Home.module.css';
+import homeImage from '@public/Home/homeImage.svg';
 
 export default function Home() {
   return (
     <section id={styles.home}>
-      <div className={styles['home__name-role']}>
-        <p className={styles.home__name}>
-          Olá 👋,
-          <br />
-          eu sou o João Victor Steteler 🇧🇷
-        </p>
-        <p className={styles.home__role}>DESENVOLVEDOR FULLSTACK</p>
+      <div className={styles['home-info']}>
+        <div className={styles['home-info__name-role']}>
+          <p className={styles['home-info__name']}>
+            Olá 👋,
+            <br />
+            eu sou o João Victor Steteler 🇧🇷
+          </p>
+          <p className={styles['home-info__role']}>DESENVOLVEDOR FULLSTACK</p>
+        </div>
+        <div>
+          <p className={styles['home-info__summary']}>
+            “Minimalista, perfeccionista, organizado,
+            regrado e professor nas horas vagas xD.”
+          </p>
+          <Link href="#about" className={styles['home-info__link']}>
+            SAIBA MAIS ➜
+          </Link>
+        </div>
+        <ContactContainer />
       </div>
-      <div>
-        <p className={styles.home__summary}>
-          “Minimalista, perfeccionista, altruísta, organizado,
-          regrado e professor nas horas vagas xD.”
-        </p>
-        <Link href="#about" className={styles.home__link}>
-          SAIBA MAIS ➜
-        </Link>
-      </div>
-      <ContactContainer />
+      <Image
+        placeholder="blur"
+        blurDataURL={homeImage.src}
+        src={homeImage.src}
+        width={480}
+        height={480}
+        className={styles.home__image}
+        quality={100}
+        alt="imagem ilustrativa de um programador no computador"
+      />
     </section>
   );
 }
